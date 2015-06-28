0c21ebe83a9d0508970df22845b130670e04c368
pawankumarbaranwal <pawankumarbaranwal@gmail.com>
2015-06-29 00:18:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pawan/progressbar/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.pawan.progressbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	TextView tv;
+	ProgressBar pBar;
+	int pStatus = 0;
+	private Handler handler = new Handler();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tv = (TextView) findViewById(R.id.textView1);
+		pBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while (pStatus <= 100) {
+
+					handler.post(new Runnable() {
+
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							pBar.setProgress(pStatus);
+							pBar.setSecondaryProgress(pStatus + 5);
+							//tv.setText(pStatus + "/" + pBar.getMax());
+						}
+					});
+					try {
+						// Sleep for 200 milliseconds.
+						// Just to display the progress slowly
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					pStatus++;
+				}
+			}
+		}).start();
+	}
+}
